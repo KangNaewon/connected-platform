6e93e0bc29d53622c1163dd7e31c863714b1872d
Toyoill <61874428+Toyoill@users.noreply.github.com>
2024-11-11 16:09:38 +0900

    Add backend template code

---
+++ test_hls/frontend/src/components/HLSVideo.js
@@ -0,0 +1,35 @@
+import {useRef, useEffect, useCallback} from 'react';
+import Button from '@enact/sandstone/Button'
+import Hls from 'hls.js';
+
+const HLSVideo = (props) => {
+	const videoRef = useRef(null);
+	const hlsRef = useRef(null);
+
+	useEffect(() => {
+		if (Hls.isSupported()) {
+			const video = videoRef.current;
+			const hls = new Hls();
+			hls.loadSource(props.src);
+			hls.attachMedia(video);
+
+			hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+				console.log(
+					'>>>>>>>>>>>> manifest loaded, found ' +
+						data.levels.length +
+						' quality level'
+				);
+				video.play();
+			});
+			hlsRef.current = hls;
+		}
+	}, [props.src]);
+
+	return (
+		<>
+			<video ref={videoRef} controls height={720} />
+		</>
+	);
+};
+
+export default HLSVideo;
--- test_hls/frontend/src/views/MainPanel.js
+++ test_hls/frontend/src/views/MainPanel.js
@@ -1,14 +1,15 @@
 import Button from '@enact/sandstone/Button';
 import kind from '@enact/core/kind';
 import {Panel, Header} from '@enact/sandstone/Panels';
+import HLSVideo from '../components/HLSVideo';
 
 const MainPanel = kind({
 	name: 'MainPanel',
 
 	render: (props) => (
 		<Panel {...props}>
-			<Header title="Hello world!" />
-			<Button>Click me</Button>
+			<Header title="HLS Video" type="compact" />
+			<HLSVideo src="https://cdn-vos-ppp-01.vos360.video/Content/HLS_HLSCLEAR/Live/channel(PPP-LL-2HLS)/index.m3u8" />
 		</Panel>
 	)
 });
