a6c6395fc5b66ceb9eb2f93ea7b963a161e9334f
이승우 <robert032626@gmail.com>
2024-11-23 01:55:48 +0900

    Add toggle to show TV stat

--- frontend/src/views/MainPanel.js
+++ frontend/src/views/MainPanel.js
@@ -1,47 +1,35 @@
 import {Panel, Header} from '@enact/sandstone/Panels';
 import Scroller from '@enact/sandstone/Scroller';
-import { useEffect, useState } from 'react';
+import {Row, Cell} from '@enact/ui/Layout';
+import Switch from '@enact/sandstone/Switch';
+import Button from '@enact/sandstone/Button';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { PROJECT_NAME } from '../constants/strings';
-import { Row, Cell, Column } from '@enact/ui/Layout';
 import MediaList from '../components/MediaList';
-import css from './MainPanel.module.less';
 
 const MainPanel = () => {
 	const restaurants = useSelector((state) => state.user.recommendations);
+	const [showTVStat, setShowTVStat] = useState(false);
+
+	const toggle = () => setShowTVStat(!showTVStat);
 
 	return (
 		<Panel>
 			<Header 
-				title={PROJECT_NAME} 
+				title={PROJECT_NAME}
+				style={{paddingRight: '50px'}}
 				slotAfter={(
 					<>
-
+					 	<Button icon='search' size='small' onClick={()=>console.log("search")}/>
+						<Button icon='profile' size='small' onClick={()=>console.log("profile")}/>
+						<Switch onClick={toggle} />
 					</>
 				)}
 			/>
-			<Row>
-				<Scroller direction='vertical' className={css.scroller}>
-					{restaurants.map((category, index) => (
-						<MediaList
-							key={index}
-							category={category.type}
-							mediaList={category.restaurants.map((restaurant) => ({
-								restaurant_id: restaurant.restaurant_id,
-								restaurant_name: restaurant.restaurant_name,
-								rating: restaurant.rating,
-								city: restaurant.city,
-								img: restaurant.img,
-							}))}
-							onClick={() => console.log("clicked")}
-						/>
-					))}
-				</Scroller>
-			</Row>
-			
-			{/* <Row>
-				<Cell>
-					<Scroller direction='vertical' className={css.scroller}>
+			<Row style={{height: '100%'}}>
+				<Cell style={{flexGrow: 1, overflow: 'hidden'}}>
+					<Scroller direction='vertical'>
 						{restaurants.map((category, index) => (
 							<MediaList
 								key={index}
@@ -58,11 +46,12 @@ const MainPanel = () => {
 						))}
 					</Scroller>
 				</Cell>
-				<Cell shrink>
-
-				</Cell>
-			</Row> */}
-
+				{showTVStat && (
+					<Cell shrink>
+						hello
+					</Cell>
+				)}
+			</Row>
 		</Panel>
 	)
 };
