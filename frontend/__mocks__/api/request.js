6c02e4e2a3f423e12d2f9c556a8fd14ded05b345
naewon <kang.naewon125@gmail.com>
2024-12-03 17:07:00 +0900

    profile mocks updated

--- frontend/__mocks__/api/request.js
+++ frontend/__mocks__/api/request.js
@@ -1,16 +1,5 @@
 import { sampleRestaurants } from "./sampleRestaurants";
 
-/* 확정된 Mock
-GET /restaurant/recommendations
-GET /restuarant/{restaurant_id}
-GET /restaurant
-POST /user/login
-POST /user/signup
-POST /user/refresh
-POST /user/refresh-token
-GET /user/{user_id}/profile
-*/
-
 export const mockAPI = async (url, method, parameters = {}) => {
   const response = {
     '/restaurant/recommendations': {
@@ -113,12 +102,27 @@ export const mockAPI = async (url, method, parameters = {}) => {
         ],
       },
     },
-
-    /* 미확정 Mocks */
+    '/profile': {
+      POST: { 
+        message: "New profile created successfully",
+        created_profile: {profile_id: '67473b04c12b4df30870272a', profile_name: 'profile_name' }
+      }
+    },
+    '/profile/{profile_id}': {
+      DELETE: {
+        message: "Profile deleted successfully"
+      }
+    },
+    '/profile/{profile_id}': {
+      PATCH: {
+          message: 'Profile name updated successfully',
+          modified_profile: { profile_id: "67473b04c12b4df30870272", "profile_name": 'profile_name',
+        }
+    },
     '/profile/switch/{profile_id}': {
       POST: {
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc0MzNjOTI5MDI3NTEyOTcwMzA0MzA4IiwiaWQiOiJrbjEyNSIsImFjdGl2ZV9wcm9maWxlX2lkIjoiNjc0MzNjOTI5MDI3NTEyOTcwMzA0MzA5IiwiaWF0IjoxNzMyNDU5OTExLCJleHAiOjE3MzI0NjM1MTF9.j31IIV02WPlzmd10KqQ1aWAJxgDBE1_Oszy2RVZ6NHo"
-      },
+      }
     },
     '/profile/{profile_id}': {
       GET: {
@@ -130,18 +134,26 @@ export const mockAPI = async (url, method, parameters = {}) => {
         ],
       },
     },
-    '/profile/{profile_id}/favorite': {
-      POST: { success: true, message: 'Added to favorites' },
+    '/profile/{profile_id}/{restaurant_id}': {
+      GET: {
+        liked: true,
+        disliked: false,
+        visited: true,
+      }
     },
-    '/profile/{profile_id}/favorite/{restaurant_id}': {
-      DELETE: { success: true, message: 'Removed from favorites' },
+    '/profile/{profile_id}/like': {
+      POST: { message: "restaurant successfully added to like list" },
+    },
+    '/profile/{profile_id}/like': {
+      DELETE: { message: 'restaurant successfully deleted from like list' },
     },
     '/profile/{profile_id}/visit': {
-      POST: { success: true, message: 'Added to visit history' },
+      POST: { message: 'restaurant successfully added to visited list' },
     },
-    '/profile/{profile_id}/visit/{restaurant_id}': {
-      DELETE: { success: true, message: 'Removed from visit history' },
+    '/profile/{profile_id}/visit': {
+      DELETE: { message: 'restaurant successfully deleted from visited list' },
     },
+    /* 미확정 Mocks */
     '/profile/{profile_id}/watch-history': {
       POST: { success: true, message: 'Watch history updated' },
     },
@@ -153,7 +165,8 @@ export const mockAPI = async (url, method, parameters = {}) => {
         url: 'https://example.com/media/cooking.mp4',
       },
     },
-  }; 2
+  }
+  };
 
   await new Promise((resolve) => setTimeout(resolve, 500));
 
