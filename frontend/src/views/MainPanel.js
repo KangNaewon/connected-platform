aee5b2a9accd83ecc2508e64fad2e4581440c61c
이승우 <robert032626@gmail.com>
2024-12-10 09:58:06 +0900

    Add Search

--- frontend/src/views/MainPanel.js
+++ frontend/src/views/MainPanel.js
@@ -1,6 +1,6 @@
-import {Panel, Header} from '@enact/sandstone/Panels';
+import { Panel, Header } from '@enact/sandstone/Panels';
 import Scroller from '@enact/sandstone/Scroller';
-import {Row, Cell} from '@enact/ui/Layout';
+import { Row, Column } from '@enact/ui/Layout';
 import Button from '@enact/sandstone/Button';
 import { useState } from 'react';
 import { PROJECT_NAME } from '../constants/strings';
@@ -15,57 +15,84 @@ import { useNavigate } from '../hooks/useNavigate';
 import { panelName } from '../constants/panelName';
 
 const MainPanel = () => {
-	const {mediaList, loading, error} = useFetchData();
+	const { mediaList, loading, error } = useFetchData();
 	const [showSearch, setShowSearch] = useState(false);
+	const [searchList, setSearchList] = useState({});
+	const [searchResult, setSearchResult] = useState(false);
 	const { isPopupOpen, handlePopupOpen, handlePopupClose, msg } = usePopup();
 	const navigate = useNavigate();
 
 	const handleMediaClick = (id) => {
-		navigate(panelName.info, {restaurant_id: id});
+		navigate(panelName.info, { restaurant_id: id });
+	}
+
+	const handleSearchBtn = () => {
+		console.log('handle search button');
+		if (!showSearch) {
+			setShowSearch(true);
+			setSearchResult(false);
+			setSearchList({});
+		} else if (showSearch && searchResult) {
+			setShowSearch(false);
+			setSearchResult(true);
+		}
 	}
 
 	if (loading) return <Loading />;
 	if (error) {
-		debugLog('Main[E]', {error: error});
+		debugLog('Main[E]', { error: error });
 		handlePopupOpen('Fail to load data');
+	}
+
+	if (searchResult && typeof searchList === 'object' && searchList.results.length === 0) {
+		setShowSearch(false);
+		setSearchResult(false);
+		handlePopupOpen('No result');
 	}
 
 	debugLog('Main[I]', mediaList);
 
 	return (
 		<Panel>
-			<Header 
+			<Header
 				title={PROJECT_NAME}
 				slotAfter={(
 					<>
-						{showSearch && (<SearchBar setShowSearch={setShowSearch} />)}
-					 	<Button icon='search' size='small' onClick={()=> setShowSearch(!showSearch)}/>
-						<Button icon='profile' size='small' onClick={()=> navigate(panelName.profile, {})}/>
-						<Button icon='dashboard1' size='small' onClick={() => navigate(panelName.dashboard, {})}/>
+						{showSearch && (<SearchBar setShowSearch={setShowSearch} setSearchResult={setSearchResult} setSearchList={setSearchList} />)}
+						<Button icon='search' size='small' onClick={handleSearchBtn} />
+						<Button icon='profile' size='small' onClick={() => navigate(panelName.profile, {})} />
+						<Button icon='dashboard1' size='small' onClick={() => navigate(panelName.dashboard, {})} />
 					</>
 				)}
 			/>
-			<Row style={{height: '100%'}}>
-				<Cell style={{flexGrow: 1, overflow: 'hidden'}}>
+			<Row style={{ height: '100%' }}>
+				<Column style={{ flexGrow: 1, overflow: 'hidden' }}>
+					{showSearch && (
+						<>
+							{searchResult === false && <Loading />}
+							{searchResult === true && typeof searchList === 'object' && searchList.results.length > 0 && (
+								<MediaList
+									key={"search-list"}
+									category={"result"}
+									mediaList={searchList.results}
+									onClick={(id) => handleMediaClick(id)}
+								/>
+							)}
+						</>
+					)}
 					<Scroller direction='vertical'>
 						{mediaList.map((category, index) => (
 							<MediaList
-								key={index}
+								key={`media-list-${category.type}-${index}`}
 								category={category.type}
-								mediaList={category.restaurants.map((restaurant) => ({
-									restaurant_id: restaurant.restaurant_id,
-									restaurant_name: restaurant.restaurant_name,
-									rating: restaurant.rating,
-									city: restaurant.city,
-									img: restaurant.img,
-								}))}
+								mediaList={category.restaurants}
 								onClick={(id) => handleMediaClick(id)}
 							/>
 						))}
 					</Scroller>
-				</Cell>
+				</Column>
 			</Row>
-			<Popup 
+			<Popup
 				isPopupOpen={isPopupOpen}
 				handlePopupClose={handlePopupClose}
 				msg={msg}
